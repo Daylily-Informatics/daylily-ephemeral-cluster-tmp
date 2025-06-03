c715705cc0427ad7578a9362fc820888085f642f
John Major <daylily@daylilyinformatics.com>
2025-06-02 18:07:12 -0700

    0.7.212

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Daylily Ephemeral Cluster
-_(stable tagged release to use --> 0.7.210)_
+_(stable tagged release to use --> 0.7.212)_
 
 **beta release**
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily-ephemeral-cluster",
-    version="0.7.210",
+    version="0.7.212",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here
